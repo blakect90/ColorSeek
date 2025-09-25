59fb682b24da43bd3682453448782be93403f626
blakect90 <blakect311@gmail.com>
2025-09-25 19:45:51 -0400

    Sliders work and give values 0-255

--- app/src/main/java/com/example/colorseek/MainActivity.java
+++ app/src/main/java/com/example/colorseek/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.colorseek;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar sb_j_Red;
+    SeekBar sb_j_Blue;
+    SeekBar sb_j_Green;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,57 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sb_j_Red = findViewById(R.id.sb_v_Red);
+        sb_j_Blue = findViewById(R.id.sb_v_Blue);
+        sb_j_Green = findViewById(R.id.sb_v_Green);
+
+        setSeekBarListeners();
+    }
+
+    public void setSeekBarListeners()
+    {
+        sb_j_Red.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sb_j_Red.setProgress(progress);
+
+                //Log.d("RED", String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        sb_j_Blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sb_j_Blue.setProgress(progress);
+                Log.d("BLUE", String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        sb_j_Green.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sb_j_Green.setProgress(progress);
+                Log.d("GREEN", String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
     }
 }
