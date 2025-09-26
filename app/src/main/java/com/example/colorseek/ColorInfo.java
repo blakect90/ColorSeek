68996ac022279e0e4682abf43850693ae90fde73
blakect90 <blakect311@gmail.com>
2025-09-26 08:38:39 -0400

    ColorInfo.class
    more work on class

--- app/src/main/java/com/example/colorseek/ColorInfo.java
+++ app/src/main/java/com/example/colorseek/ColorInfo.java
@@ -1,5 +1,7 @@
 package com.example.colorseek;
 
+import java.util.ArrayList;
+
 public class ColorInfo
 {
     private int red;
@@ -22,6 +24,25 @@ public class ColorInfo
         blue = b;
         luminance = l;
         hex = h;
+    }
+    //GETTERS AND SETTERS==============================================================================
+    public int getRed() {return red;}
+    public void setRed(int red) {this.red = red;}
+    public int getGreen() {return green;}
+    public void setGreen(int green) {this.green = green;}
+    public int getBlue() {return blue;}
+    public void setBlue(int blue) {this.blue = blue;}
+    public double getLuminance() {return luminance;}
+    public void setLuminance(double luminance) {this.luminance = luminance;}
+    public String getHex() {return hex;}
+    public void setHex(String hex) {this.hex = hex;}
+    //==============================================================================================
+
+    static class savedColor
+    {
+        static ArrayList<String> savedColor = new ArrayList<>();
+        public static String savedColorAt(int i) {return savedColor.get(i);}
+        public static void addSavedColor
     }
 }
 
